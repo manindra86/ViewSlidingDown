9bc0394e6888c3d4a1bfdb79c4db0ab24f1c8f97
Manindra <manindra@ABCs-iMac.local>
2015-05-14 13:09:29 +0530

    Initial Commit

---
+++ ImageCombine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageCombine
+//
+//  Created by Manindra on 14/05/15.
+//  Copyright (c) 2015 10C Internet Pvt. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
